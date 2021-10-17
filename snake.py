1ae79ad7e95a1556f0ddccd351f257ebbb4e13ac
Степан Герасимчук <stepangerasimcuk@MacBook-Air-Stepan.local>
2021-10-17 11:53:11 +1000

    moves and comment window center

--- snake.py
+++ snake.py
@@ -34,8 +34,31 @@ class Food:
 
         canvas.create_oval(x, y, x + SPACE_SIZE, y+SPACE_SIZE,fill=FOOD_COLOR,tag ='food' )
 
-def next_turn():
-    pass
+def next_turn(snake,food):
+    
+    x,y = snake.coordinates[0]
+
+    if direction == 'up':
+        y-= SPACE_SIZE
+    elif direction == 'down':
+        y += SPACE_SIZE
+    elif direction == 'left':
+        x-= SPACE_SIZE
+    elif direction == 'right':
+        x+=SPACE_SIZE
+
+    snake.coordinates.insert(0,(x,y))
+    square = canvas.create_rectangle(x,y, x+ SPACE_SIZE,y+SPACE_SIZE,fill=SNAKE_COLOR)
+
+    snake.squares.insert(0,square)
+
+    del snake.coordinates[-1]
+
+    canvas.delete(snake.squares[-1])
+
+    del snake.squares[-1]
+
+    window.after(SPEED,next_turn,snake,food)
 
 def change_direction(new_direction):
     pass
@@ -64,17 +87,19 @@ canvas.pack()
 
 window.update()
 
-window_width = window.winfo_width()
-window_height = window.winfo_height()
-screen_width = window.winfo_screenmmwidth()
-screen_height = window.winfo_screenheight()
+# window_width = window.winfo_width()
+# window_height = window.winfo_height()
+# screen_width = window.winfo_screenmmwidth()
+# screen_height = window.winfo_screenheight()
 
-x = int((screen_width/2)-(window_width/2))
-y = int((screen_height/2)-(window_height/2))
+# x = int((screen_width/2)-(window_width/2))
+# y = int((screen_height/2)-(window_height/2))
 
-window.geometry(f'{window_width}x{window_height}+{x}+{y}')
+# window.geometry(f'{window_width}x{window_height}+{x}+{y}')
 
 snake = Snake()
 food = Food()
+
+next_turn(snake,food)
 
 window.mainloop()
