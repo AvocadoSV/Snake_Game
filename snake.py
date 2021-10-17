eb22f3ab279b31acd37611c283e199ff681b60ea
Степан Герасимчук <stepangerasimcuk@MacBook-Air-Stepan.local>
2021-10-17 11:20:42 +1000

    constants

---
+++ snake.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = 'GREEN'
+FOOD_COLOR = 'RED'
+BG_COLOR = 'BLACK'
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
