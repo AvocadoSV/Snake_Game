12fbe94cab2cbf76057dcc35b94c9dd74735a8b0
Степан Герасимчук <stepangerasimcuk@MacBook-Air-Stepan.local>
2021-10-17 11:43:09 +1000

    food

--- snake.py
+++ snake.py
@@ -14,8 +14,14 @@ class Snake:
     pass
 
 class Food:
-    pass
+    def __init__(self):
 
+        x = random.randint(0,(GAME_WIDTH/SPACE_SIZE)-1)*SPACE_SIZE
+        y = random.randint(0,(GAME_HEIGHT/SPACE_SIZE)-1)*SPACE_SIZE
+
+        self.coordinates = [x,y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y+SPACE_SIZE,fill=FOOD_COLOR,tag ='food' )
 
 def next_turn():
     pass
@@ -47,14 +53,17 @@ canvas.pack()
 
 window.update()
 
-window_width = window.winfo_width()
-window_height = window.winfo_height()
-screen_width = window.winfo_screenmmwidth()
-screen_height = window.winfo_screenheight()
+# window_width = window.winfo_width()
+# window_height = window.winfo_height()
+# screen_width = window.winfo_screenmmwidth()
+# screen_height = window.winfo_screenheight()
 
-x = int((screen_width/2)-(window_width/2))
-y = int((screen_height/2)-(window_height/2))
+# x = int((screen_width/2)-(window_width/2))
+# y = int((screen_height/2)-(window_height/2))
 
-window.geometry(f'{window_width}x{window_height}+{x}+{y}')
+# window.geometry(f'{window_width}x{window_height}+{x}+{y}')
+
+snake = Snake()
+food = Food()
 
 window.mainloop()
