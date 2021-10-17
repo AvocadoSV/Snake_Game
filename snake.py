27bd7a5a6e7997a7714ed4afe9c800b46000a722
Степан Герасимчук <stepangerasimcuk@MacBook-Air-Stepan.local>
2021-10-17 12:10:02 +1000

    collisions

--- snake.py
+++ snake.py
@@ -70,7 +70,10 @@ def next_turn(snake,food):
 
         del snake.squares[-1]
 
-    window.after(SPEED,next_turn,snake,food)
+    if check_collisions(snake):
+        game_over()
+    else:
+        window.after(SPEED,next_turn,snake,food)
 
 def change_direction(new_direction):
     global direction
@@ -88,8 +91,19 @@ def change_direction(new_direction):
         if direction !='up':
             direction = new_direction
         
-def check_collisions():
-    pass
+def check_collisions(snake):
+    x,y = snake.coordinates[0]
+
+    if x < 0 or x >= GAME_WIDTH:
+        return True
+    elif y < 0 or y >= GAME_HEIGHT:
+        return True
+    
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
 
 def game_over():
     pass
